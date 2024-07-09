be61b3a7a7b846683a3d1e950a4fd3013d680f58
The-EverChosen <Meier-bewerbung@gmx.de>
2024-07-09 10:51:39 +0200

    Projektdateien hinzufügen.

---
+++ Image_Viewer/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Image_Viewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void openImage(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void selectStretchMode(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
